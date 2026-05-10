d2ff2324bb9b7fbe577b5bbd72b66ef6bd3c3b4c
cirosanchez <ciroandressanchezbuitrago@gmail.com>
2026-05-09 22:57:46 -0500

    Created the main function. Loades configuration and gives the theme colors to slint.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+mod config;
+mod theme_manager;
 
 use std::error::Error;
+
+use config::Settings;
+use crate::theme_manager::ThemeManager;
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let settings = Settings::load();
+
     let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
+    ThemeManager::apply(&ui, &settings);
 
     ui.run()?;
 
     Ok(())
 }
+
